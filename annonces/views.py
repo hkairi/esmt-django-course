fb4ab33aa82c4b1261326dda495e37be106fa8b2
Hassane Moustapha <hassanemoustapha@gmail.com>
2018-02-08 18:29:01 +0000

    Ajout d'une route /annonces/:id + action dans views

--- annonces/views.py
+++ annonces/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import Annonce
+
+
 def index(request):
     context = {
         'annonces': Annonce.objects.all()
     }
     return render(request, "index.html", context)
+
+
+def show(request, id):
+    context = {
+        'annonce': Annonce.objects.get(id=id)
+    }
+    return render(request, "show.html", context)
