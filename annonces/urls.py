9003695d5995ad7c534c6078205bc8c7e2919532
Hassane Moustapha <hassanemoustapha@gmail.com>
2018-02-13 18:22:52 +0000

    Ajout de categories

--- annonces/urls.py
+++ annonces/urls.py
@@ -5,6 +5,7 @@ app_name = 'annonces'
 
 urlpatterns = [
     path('', views.index),
+    path('categories/<int:catId>/annonces', views.categorie, name='categorie'),
     path('annonces/', views.index),
     path('annonces/<int:id>', views.show, name='details'),
 ]
--- annonces/views.py
+++ annonces/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import Annonce
+from .models import Annonce, Categorie
 
 
 def index(request):
     context = {
-        'annonces': Annonce.objects.all()
+        'annonces': Annonce.objects.all(),
+        'categories': Categorie.objects.all()
     }
     return render(request, "index.html", context)
 
@@ -14,3 +15,11 @@ def show(request, id):
         'annonce': Annonce.objects.get(id=id)
     }
     return render(request, "show.html", context)
+
+
+def categorie(request, catId):
+    context = {
+        'categorie': Categorie.objects.get(id=catId),
+        'annonces': Annonce.objects.filter(categorie_id=catId, publiee=True)
+    }
+    return render(request, "show-categorie.html", context)
